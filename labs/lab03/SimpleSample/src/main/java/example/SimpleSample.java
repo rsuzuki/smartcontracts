0d11d47271d66cc9d797f191186e4e1ee4a7c2fd
rsuzuki <rsuzuki.okada@gmail.com>
2016-12-13 14:31:50 -0200

    Update SimpleSample.java
    chaincode id updated.

--- labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
+++ labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
@@ -150,7 +150,7 @@ public class SimpleSample extends ChaincodeBase {
 
 	@Override
 	public String getChaincodeID() {
-		return "rokada-chaincode-id";
+		return "rokadaSimpleSample";
 	}
 
 	public static void main(String[] args) throws Exception {
